9b27aee2432adfa71778f9230e2eab16dd8d879e
Navya <navyap942@icloud.com>
2025-11-04 22:42:26 +0530

    Update files

--- firebase.js
+++ firebase.js
@@ -1,17 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyDuQULtYoC4rBUmHN9Kg1MVPPqLQS9Vy1Y",
-  authDomain: "cloud-collab.firebaseapp.com",
-  projectId: "cloud-collab",
-  storageBucket: "cloud-collab.firebasestorage.app",
-  messagingSenderId: "669000546229",
-  appId: "1:669000546229:web:0d03a7465b2e6d921a4bfc"
+    apiKey: "AIzaSyDuQULtYoC4rBUmHN9Kg1MVPPqLQS9Vy1Y",
+    authDomain: "cloud-collab.firebaseapp.com",
+    projectId: "cloud-collab",
+    storageBucket: "cloud-collab.firebasestorage.app",
+    messagingSenderId: "669000546229",
+    appId: "1:669000546229:web:0d03a7465b2e6d921a4bfc"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+// Auth Reference
+const auth = firebase.auth();
